8dd6f80265c6a6e76a304f7ae6fd5750c6287f1f
ysilksa <142238359+ysilksa@users.noreply.github.com>
2024-07-08 12:14:04 -0500

    Added NavigationLinks and hard-coded pins.
    - NavigationLinks added with a NavigationView. 
    - Hard-coded pins with an event name, a mock image, and an event description. 
    - Work location changed.

--- girlcode/girlcode/MapView.swift
+++ girlcode/girlcode/MapView.swift
@@ -2,27 +2,23 @@
 //  Map.swift
 //  girlcode
 //
-//  Created by 3 GO Participant on 7/2/24.
+//  Created by Chisa Yan on 7/2/24.
 //
 
 import SwiftUI
 import MapKit
 
+// hard-coded example events, where each item in the list represents one event, its name/description, and its coordinates.
 let MapLocations = [
-    MapViewLocations(eventName: "Car Accident", eventDescription: "License plate ABCDEF was damaged in a hit-and-run", latitude: 41.8795, longitude: -87.6338)
+    MapViewLocation(eventName: "Car Accident", eventDescription: "License plate ABCDEF was damaged in a hit-and-run near Adams St and Wells St.", latitude: 41.8795, longitude: -87.6338),
+    MapViewLocation(eventName: "Aggravated Assault", eventDescription: "Woman in mid-thirties assaulted on Michigan and Illinois by person wearing black.", latitude: 41.890965, longitude: -87.623936),
+    MapViewLocation(eventName: "Motor Vehicle Theft", eventDescription: "License plate number ABCDEF found with a broken window.", latitude: 42.056206, longitude: -87.677219),
+    MapViewLocation(eventName: "Suspicious Person", eventDescription: "Suspicious person spotted between Lunt Hall and Swift Hall.", latitude: 42.055054, longitude: -87.675612),
+    MapViewLocation(eventName: "Suspicious Person", eventDescription: "Suspicious person spotted between Wacker Dr and Monroe.", latitude: 41.880584, longitude: -87.636623)
 ]
 
 struct MapView: View {
-    // TODO: add custom pins on the map
-    
     // the starting position of the camera, Chicago
-//    @State private var startPos = MapCameraPosition.region(
-//        MKCoordinateRegion(
-//            center: CLLocationCoordinate2D(latitude: 41.881832, longitude: -87.623177),
-//            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-//        )
-//    )
-    
     @State private var startPos = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 41.881832, longitude: -87.623177),
         span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
@@ -35,14 +31,24 @@ struct MapView: View {
             backgroundGradient
             
             // Map set to the startPos of Chicago
-            //Map(position: $startPos)
-            
-            Map(coordinateRegion: $startPos, annotationItems: MapLocations
-            ) { place in
-                MapAnnotation(coordinate: place.coordinate) {
-                    PlaceAnnotationView(title: place.eventName)
+            // allows for navigation to a separate tab when the pin is pressed on
+            NavigationView {
+                Map(coordinateRegion: $startPos, annotationItems: MapLocations
+                ) { place in
+                    MapAnnotation(coordinate: place.coordinate) {
+                      NavigationLink {
+                        LocationEventView(event: place)
+                      } label: {
+                        PlaceAnnotationView(event: place)
+                      }
+                    }
                 }
+                
             }
+            .ignoresSafeArea(edges: .top)
+            .navigationTitle("Custom Annotation")
+            .navigationBarTitleDisplayMode(.inline)
+            
             
             // white background for tabview
             whiteTabView
@@ -65,7 +71,7 @@ struct MapView: View {
                     startPos =
                         MKCoordinateRegion(
                             center: CLLocationCoordinate2D(latitude: 41.878876, longitude: -87.635918),
-                            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                            span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
                         )
                     
                 }
@@ -87,8 +93,8 @@ struct MapView: View {
                 Button("Work") { // change the coordinates to the Apple Michigan Avenue Store
                     startPos =
                         MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 41.898773, longitude: -87.622925),
-                            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                            center: CLLocationCoordinate2D(latitude: 41.889505, longitude: -87.623056),
+                            span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
                         )
                     
                 }
@@ -110,7 +116,7 @@ struct MapView: View {
                     startPos =
                         MKCoordinateRegion(
                             center: CLLocationCoordinate2D(latitude: 42.055984, longitude: -87.675171),
-                            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                            span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
                         )
                     
                 }
@@ -121,6 +127,7 @@ struct MapView: View {
             
         }
         .ignoresSafeArea()
+       
     }
 }
 
@@ -129,7 +136,7 @@ struct MapView: View {
 }
 
 // struct for a MapViewLocation
-struct MapViewLocations: Identifiable {
+struct MapViewLocation: Identifiable {
     let id = UUID()
     let eventName: String // name of the event, i.e. "Car crash"
     let eventDescription: String // description of the event, i.e. "Car with license plate ABCDEF was hit."
@@ -143,13 +150,16 @@ struct MapViewLocations: Identifiable {
 
 // struct for a custom pin
 struct PlaceAnnotationView: View {
+    // allows the name to be toggled on and off
     @State private var showTitle = true
     
-    let title: String
+    let event: MapViewLocation
     
     var body: some View {
         VStack(spacing: 0) {
-            Text(title)
+            VStack (spacing: 0) {
+                Text(event.eventName)
+            }
                 .font(.callout)
                 .padding(5)
                 .background(Color(.white))
@@ -167,10 +177,85 @@ struct PlaceAnnotationView: View {
                 .offset(x: 0, y: -5)
         }
         .onTapGesture {
-            withAnimation(.easeInOut) {
+              withAnimation(.easeInOut) {
                 showTitle.toggle()
-            }
+              }
         }
         
     }
 }
+
+// struct for a detailed event when you click for further details
+struct LocationEventView: View {
+    
+    // var for the center of the x-value
+    @State var centerCoord = 196.5
+    
+    // input variable for an event
+    let event : MapViewLocation
+    
+    var body : some View {
+        ZStack {
+            // the background of the navigation view
+            backgroundGradient
+            
+            VStack (spacing: 30) {
+                // border and text for the details of the event
+                ZStack {
+                    // color background for eventName
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 15655915))
+                        .frame(width: 250, height: 70)
+                    
+                    // white background for eventName
+                    RoundedRectangle (cornerRadius: 10)
+                        .fill(Color.white)
+                        .frame(width: 240, height: 60)
+                    
+                    // text for the eventName
+                    Text(event.eventName)
+                        .font(.title)
+                        .foregroundStyle(Color(hex: 2500134))
+                }
+                
+                // example image of a crossroads, could represent any image of the situation
+                VStack {
+                    Image(.crossroadsBackground)
+                        .resizable()
+                        .frame(width: 270, height: 200)
+                        .border(Color(hex: 15655915), width: 4)
+                    
+                    // attributing the creator
+                    Text("Designed by Freepik")
+                        .font(.caption)
+                }
+                
+                // border and text for details of the event
+                ZStack {
+                    // color background for eventDescription
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 15655915))
+                        .frame(width: 250, height: 160)
+                    
+                    // white background for eventDescription
+                    RoundedRectangle (cornerRadius: 10)
+                        .fill(Color.white)
+                        .frame(width: 240, height: 150)
+                    
+                    // text for the eventDescription
+                    Text(event.eventDescription)
+                        .font(.title3)
+                        .frame(width: 200, height: 150)
+                        .foregroundStyle(Color(hex: 2500134))
+
+                }
+            }
+            .position(CGPoint(x: centerCoord, y: 290))
+        }
+        .navigationTitle("Further Details") // title at the top of the page
+        
+    }
+    
+}
+
+
