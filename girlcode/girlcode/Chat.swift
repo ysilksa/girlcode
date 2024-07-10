28a716d221b34265cea42dc986ad639b1f25c24c
ysilksa <142238359+ysilksa@users.noreply.github.com>
2024-07-10 12:06:48 -0500

    Add Chat.swift from teammate.

--- girlcode/girlcode/Chat.swift
+++ girlcode/girlcode/Chat.swift
@@ -3,27 +3,40 @@
 //  girlcode
 //
 //  Created by 3 GO Participant on 6/29/24.
-// This swift file holds the page for the chat, where users can send messages with different tags to specify their messages' purpose. 
+// This swift file holds the page for the chat, where users can send messages.
 //
 
 import SwiftUI
 
 struct Chat: View {
+    @StateObject var messagesManager = MessagesManager()
     var body: some View {
-        ZStack {
-            // background gradient color
-            backgroundGradient
+        VStack {
+            VStack {
+               TileRow()
+                    ScrollViewReader { proxy in
+                    ScrollView{
+                        ForEach(messagesManager.messages, id: \.id){message in
+                            MessageBubble(message: message)
+                        }
+                    }
+                    .padding(.top, 10)
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                    .background(.white)
+                    .cornerRadius(30, corners: [.topLeft, .topRight])
+                    .onChange(of: messagesManager.lastMessageId) { id in
+                      withAnimation {
+                          proxy.scrollTo(id, anchor: .bottom)
+                          }
+                     }
+                }
+            }
+            .background(Color("Gray"))
             
-            // white background for tabview
-            whiteTabView
-                .frame(width: 400, height: 116.0)
-                .foregroundColor(.white)
-                .position(CGPoint(x: 196.5, y: 800))
-            
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            MessageField()
+                .environmentObject(MessagesManager())
         }
-        .ignoresSafeArea()
-        
     }
 }
 
