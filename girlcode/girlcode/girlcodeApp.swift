bccdbc30a6d812b9af8aa72c792847668a107869
ysilksa <142238359+ysilksa@users.noreply.github.com>
2024-07-10 12:12:32 -0500

    Update to allow Firebase.

--- girlcode/girlcode/girlcodeApp.swift
+++ girlcode/girlcode/girlcodeApp.swift
@@ -2,13 +2,19 @@
 //  girlcodeApp.swift
 //  girlcode
 //
-//  Created by 3 GO Participant on 6/29/24.
+//  Created by Chisa Yan on 6/29/24.
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct girlcodeApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
